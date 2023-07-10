385d156c5afa174570447593a5f7613730e22170
prasenjitGDL <prasenjit.chakraborty@overdose.digital>
2023-07-10 12:01:00 +0530

    task 1

--- Task01_project_setup.ts
+++ Task01_project_setup.ts
@@ -8,10 +8,29 @@ import { apiRoot } from "./handson/client";
 // https://docs.commercetools.com/api/projects/project#get-project
 
 // TODO: Get project settings
+const getProject = () => {
+  return apiRoot.get().execute();
+};
 
+getProject()
+  .then((res) => log(res))
+  .catch((error) => log(error));
 
 // TODO: Get shipping method by id
+// const getShippingMethod = (id: string) => {
+//   return apiRoot.shippingMethods().withId({ ID: id }).get().execute();
+// };
+
+// getShippingMethod("shipping_id")
+//   .then((res) => log(res))
+//   .catch((error) => log(error));
 
 
 // TODO: Get standard tax category by key
+// const getTaxCategory = (key: string) => {
+//     return apiRoot.taxCategories().withKey({key}).get().execute()
+// }
 
+// getTaxCategory("tax_key")
+//   .then((res) => log(res))
+//   .catch((error) => log(error));
--- handson/client.ts
+++ handson/client.ts
@@ -1,29 +1,65 @@
 import { createApiBuilderFromCtpClient as createImportApiBuilderFromCtpClient } from "@commercetools/importapi-sdk";
 import { createApiBuilderFromCtpClient } from "@commercetools/platform-sdk";
-import { AuthMiddlewareOptions, ClientBuilder, HttpMiddlewareOptions, PasswordAuthMiddlewareOptions } from "@commercetools/sdk-client-v2";
+import {
+  AuthMiddlewareOptions,
+  ClientBuilder,
+  HttpMiddlewareOptions,
+  PasswordAuthMiddlewareOptions,
+} from "@commercetools/sdk-client-v2";
 import fetch from "node-fetch";
 import { ApiRoot, ImportApiRoot } from "../types/global";
 import { Prefix, Config, readConfig } from "../utils/config";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: `https://auth.${readConfig().region}.commercetools.com`,
+  projectKey: readConfig().projectKey,
+  credentials: {
+    clientId: readConfig().clientId,
+    clientSecret: readConfig().clientSecret,
+  },
+  scopes: [readConfig().scope],
+  fetch,
+};
+
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: `https://api.${readConfig().region}.commercetools.com`,
+  includeOriginalRequest: true,
+  ...fetch,
+};
+
+const client = new ClientBuilder()
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withProjectKey(readConfig().projectKey)
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withLoggerMiddleware()
+  .build();
 
 const createApiClient = () => {
-    throw new Error("Function not implemented");
-}
+  const apiClient = createApiBuilderFromCtpClient(client).withProjectKey({
+    projectKey: readConfig().projectKey,
+  });
+  return apiClient;
+};
 
-const createImportApiClient = () => {
-    throw new Error("Function not implemented");
-}
+// const createImportApiClient = () => {
+//     const importApiClient = createImportApiBuilderFromCtpClient(client);
+//     return importApiClient;
+// }
 
-const createStoreApiClient = () => {
-    throw new Error("Function not implemented");
-}
+// const createStoreApiClient = () => {
+//     const storeApiClient = createApiBuilderFromCtpClient(client);
+//     return storeApiClient;
+// }
 
-const createMyApiClient = () => {
-    throw new Error("Function not implemented");
-}
+// const createMyApiClient = () => {
+//     const myApiClient = createApiBuilderFromCtpClient(client);
+//     return myApiClient;
+// }
 
-
-export const apiRoot: ApiRoot = createApiClient();
-// export const importApiRoot: ImportApiRoot = createImportApiClient();
-// export const storeApiRoot: ApiRoot = createStoreApiClient();
-// export const myApiRoot: ApiRoot = createMyApiClient();
+export const apiRoot = createApiClient();
+// export const importApiRoot = createImportApiClient();
+// export const storeApiRoot = createStoreApiClient();
+// export const myApiRoot = createMyApiClient();
